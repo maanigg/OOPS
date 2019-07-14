5897ef0fc9fd333f3c5bcbecf93d193bde3fffaa
maanigg <52887003+maanigg@users.noreply.github.com>
2019-07-14 23:12:00 +0530

    Update 4a.c

--- 4a.c
+++ 4a.c
@@ -1,10 +1,10 @@
 #include<iostream> 
 using namespace std; 
-template<class t>               //Define a template function having data type t 
-void bubble(t a[30],int n)      //Bubble sort function 
+template<class t>              
+void bubble(t a[30],int n)      
 { 
         int i,j; 
-        t temp;                 //Declare temp of data type t 
+        t temp;                
         for(i=0;i<n-1;i++) 
                 for(j=0;j<n-i-1;j++) 
         { 
@@ -15,7 +15,7 @@ void bubble(t a[30],int n)      //Bubble sort function
                         a[j+1]=temp; 
                 } 
         }//End of sorting 
-        cout<<"The sorted array is"<<endl;      //Display the sorted array 
+        cout<<"The sorted array is"<<endl;   
         for(i=0;i<n;i++) 
                 cout<<a[i]<<" "; 
         cout<<endl; 
@@ -28,19 +28,19 @@ main()
         float c[30]; 
         cout<<"Enter the number of integer elements in array"<<endl; 
         cin>> n; 
-        cout<<"Enter the integer elements"<<endl;       //Input the integer type elements 
+        cout<<"Enter the integer elements"<<endl;       
         for(i=0;i<n;i++) 
                 cin>>a[i]; 
         bubble(a,n);            //Bubble sort for integer data type 
         cout<<"Enter the number of char elements in array"<<endl; 
         cin>>m; 
-        cout<<"Enter the char elements"<<endl;          //Input the char type elements 
+        cout<<"Enter the char elements"<<endl;          
                 for(i=0;i<m;i++) 
                         cin>>b[i]; 
         bubble(b,m);            //Bubble sort for char data type 
         cout<<"Enter the number of floating type elements in array"<<endl; 
         cin>>p; 
-        cout<<"Enter the floating type elements"<<endl; //Input the float type elements 
+        cout<<"Enter the floating type elements"<<endl; 
                 for(i=0;i<p;i++) 
                 cin>>c[i]; 
         bubble(c,p);        
